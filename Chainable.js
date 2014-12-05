4f62ab0cae292452b1fb7352656f9a9f556b5c3c
Marcin Batkowski <arseniew@gmail.com>
2014-12-05 12:29:25 +0100

    Changed behaviour: Instead of storing initial value directly, shallow copy is created when value is either Array or Object

--- Chainable.js
+++ Chainable.js
@@ -1,15 +1,34 @@
-(function() {
+(function () {
     'use strict';
     var root = this;
     var previous = root.Chainable;
 
+    function copyShallow(source) {
+        var
+            objCopy = {},
+            prop;
+        if (typeof source !== 'object') {
+            return source;
+        }
+        else if (Object.prototype.toString.call(source) === '[object Array]') {
+            return source.slice();
+        }
+        for (prop in source) {
+            if (source.hasOwnProperty(prop)) {
+                objCopy[prop] = source[prop];
+            }
+        }
+        return objCopy;
+    }
+
     root.Chainable = (function () {
 
         function Chainable(initialStorage) {
-            var storage = initialStorage;
+            var storage;
             if (!(this instanceof Chainable)) {
                 return new Chainable(initialStorage);
             }
+            storage = copyShallow(initialStorage);
 
             this.chain = function (cb, extraArguments, context) {
                 storage = cb.apply(context || this, [storage].concat(extraArguments));
